1bb3290046be2b192e128b7d50a51861c4bfd55d
CODSensei <krishna2342005@gmail.com>
2024-07-21 17:13:59 +0530

    completed reactWeather App

--- src/App.js
+++ src/App.js
@@ -1,15 +1,43 @@
-import './App.css';
-import Search from './components/search/Search';
+import { useState } from "react";
+import { WEATHER_API_KEYS, WEATHER_API_URL } from "./api";
+import "./App.css";
+import CurrentWeather from "./components/current-weather/Current-Weather";
+import Search from "./components/search/Search";
+import Forecast from "./components/forecast/Forecast";
 
 function App() {
+  const [currentWeather, setCurrentWeather] = useState(null);
+  const [forecastWeather, setForecastWeather] = useState(null);
 
   const handleOnSearchChange = (searchData) => {
-    console.log(searchData);
-  }
+    const [latitude, longitude] = searchData.value.split(" ");
+
+    const currentWeatherFetch = fetch(
+      `${WEATHER_API_URL}/weather?lat=${latitude}&lon=${longitude}&appid=${WEATHER_API_KEYS}&units=metric`
+    );
+
+    const forecastFetch = fetch(
+      `${WEATHER_API_URL}/forecast?lat=${latitude}&lon=${longitude}&appid=${WEATHER_API_KEYS}&units=metric`
+    );
+
+    Promise.all([currentWeatherFetch, forecastFetch])
+      .then(async (response) => {
+        const weatherResponse = await response[0].json();
+        const forecastResponse = await response[1].json();
+        setCurrentWeather({ city: searchData.label, ...weatherResponse });
+        setForecastWeather({ city: searchData.label, ...forecastResponse });
+      })
+      .catch((err) => console.log(err));
+  };
+
+  console.log(currentWeather);
+  console.log(forecastWeather);
 
   return (
     <div className="container">
-      <Search onSearchChange={handleOnSearchChange}/>
+      <Search onSearchChange={handleOnSearchChange} />
+      {currentWeather && <CurrentWeather data={currentWeather} />}
+      {forecastWeather && <Forecast data={forecastWeather} />}
     </div>
   );
 }
--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -2,33 +2,41 @@ import { useState } from "react";
 import { AsyncPaginate } from "react-select-async-paginate";
 import { GEO_API_URL, geoApiOptions } from "../../api";
 
-const Search = ({onSearchChange}) => {
+const Search = ({ onSearchChange }) => {
+  const [search, setSearch] = useState(null);
 
-    const [search, setSearch] = useState(null);
-
-    const loadOptions = async(inputValue) => {
-        try{
-            const response = await fetch(`${GEO_API_URL}/Cities`, geoApiOptions);
-            const result = await response.text();
-            console.log(result);
-        } catch (error) {
-            console.error(error);
-        }
-    }
-
-    const handleOnChange = (searchData) => {
-        setSearch(searchData);
-        onSearchChange(searchData);
-    }
-
-    return (
-        <AsyncPaginate 
-            placeholder="Search for Cities"
-            debounceTimeout={600}
-            value={search}
-            onChange={handleOnChange}
-            loadOptions={loadOptions}
-        />
+  const loadOptions = (inputValue) => {
+    return fetch(
+      `${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`,
+      geoApiOptions
     )
-} 
+      .then((response) => response.json())
+      .then((response) => {
+        return {
+          options: response.data.map((city) => {
+            return {
+              value: `${city.latitude} ${city.longitude}`,
+              label: `${city.name}, ${city.countryCode}`,
+            };
+          }),
+        };
+      })
+      .catch((err) => console.error(err));
+  };
+
+  const handleOnChange = (searchData) => {
+    setSearch(searchData);
+    onSearchChange(searchData);
+  };
+
+  return (
+    <AsyncPaginate
+      placeholder="Search for Cities"
+      debounceTimeout={600}
+      value={search}
+      onChange={handleOnChange}
+      loadOptions={loadOptions}
+    />
+  );
+};
 export default Search;
